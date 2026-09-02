a5f81be656235bde269bda52e56bed9e3ae20a19
Angelica  Julieth Silva  Perez <angelica.silva01@globant.com>
2026-09-02 10:05:41 -0500

    Create CartPage class

---
+++ src/test/java/com/saucedemo/pages/CheckoutStepOnePage.java
@@ -0,0 +1,11 @@
+package com.saucedemo.pages;
+
+import com.saucedemo.base.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutStepOnePage extends BasePage {
+
+    public CheckoutStepOnePage(WebDriver driver) {
+        super(driver);
+    }
+}
