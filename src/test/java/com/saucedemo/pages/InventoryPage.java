9b4e76cb2015a9cce3d7958fe81396663093b363
Angelica  Julieth Silva  Perez <angelica.silva01@globant.com>
2026-09-02 09:43:09 -0500

    Create InventoryPage class

---
+++ src/test/java/com/saucedemo/pages/InventoryPage.java
@@ -0,0 +1,35 @@
+package com.saucedemo.pages;
+
+import com.saucedemo.base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class InventoryPage extends BasePage {
+
+    @FindBy(className = "shopping_cart_link")
+    private WebElement cartIcon;
+
+    @FindBy(css = "button[class*='btn_inventory']")
+    private List<WebElement> addToCartButtons;
+
+    public InventoryPage(WebDriver driver) {
+            super(driver);
+    }
+
+    public void addRandomProductToCart() {
+        int randomIndex = new Random().nextInt(addToCartButtons.size());
+        addToCartButtons.get(randomIndex).click();
+    }
+
+    public void addProductToCartByIndex(int index) {
+        addToCartButtons.get(index).click();
+    }
+
+    public CartPage goToCart() {
+        cartIcon.click();
+        return new CartPage(driver);
+    }
+
+}
