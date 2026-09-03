e6c7c5ba9b37bd0910c3af73ad1de9e67d523e13
Angelica  Julieth Silva  Perez <angelica.silva01@globant.com>
2026-09-03 11:55:21 -0500

    Create logout method

--- src/test/java/com/saucedemo/base/BasePage.java
+++ src/test/java/com/saucedemo/base/BasePage.java
@@ -1,15 +1,34 @@
 package com.saucedemo.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
 
     protected WebDriver driver;
+
+    @FindBy(id = "react-burger-menu-btn")
+    private WebElement menuButton;
+
+    @FindBy(id = "logout_sidebar_link")
+    private WebElement logoutLink;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
+    public com.saucedemo.pages.LoginPage logout() {
+        menuButton.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(logoutLink));
+        logoutLink.click();
+        return new com.saucedemo.pages.LoginPage(driver);
+    }
 }
