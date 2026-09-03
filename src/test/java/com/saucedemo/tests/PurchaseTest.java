2b78820a66ce31a54167160db3c16127eaec04e8
Angelica  Julieth Silva  Perez <angelica.silva01@globant.com>
2026-09-03 12:10:06 -0500

    Create PurchaseTest class

---
+++ src/test/java/com/saucedemo/tests/PurchaseTest.java
@@ -0,0 +1,25 @@
+package com.saucedemo.tests;
+
+import com.saucedemo.base.BaseTest;
+import com.saucedemo.pages.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PurchaseTest extends BaseTest{
+
+    @Test
+    public void purchaseProductSuccessfully() {
+        LoginPage loginPage = new LoginPage(driver);
+        InventoryPage inventoryPage = loginPage.login("standard_user", "secret_sauce");
+
+        inventoryPage.addRandomProductToCart();
+        CartPage cartPage = inventoryPage.goToCart();
+
+        CheckoutStepOnePage stepOnePage = cartPage.checkout();
+        CheckoutStepTwoPage stepTwoPage = stepOnePage.fillPersonalInfo("Angelica", "Silva", "110110");
+        CheckoutCompletePage completePage = stepTwoPage.finish();
+
+        Assert.assertEquals(completePage.getThankYouMessage(), "Thank you for your order!",
+                "The confirmation message was not displayed correctly.");
+    }
+}
