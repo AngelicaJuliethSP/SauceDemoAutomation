1e2ae150af5829fcbec6b6475792af4acd0922ea
Angelica  Julieth Silva  Perez <angelica.silva01@globant.com>
2026-09-03 12:20:07 -0500

    Create RemoveCartItemsTest class

--- src/test/java/com/saucedemo/tests/RemoveCartItemsTest.java
+++ src/test/java/com/saucedemo/tests/RemoveCartItemsTest.java
@@ -1,4 +1,29 @@
 package com.saucedemo.tests;
 
-public class RemoveCartItemsTest {
+import com.saucedemo.base.BaseTest;
+import com.saucedemo.pages.CartPage;
+import com.saucedemo.pages.InventoryPage;
+import com.saucedemo.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RemoveCartItemsTest extends BaseTest{
+
+    @Test
+    public void removeAllItemsFromCart() {
+        LoginPage loginPage = new LoginPage(driver);
+        InventoryPage inventoryPage = loginPage.login("standard_user", "secret_sauce");
+
+        inventoryPage.addProductToCartByIndex(0);
+        inventoryPage.addProductToCartByIndex(1);
+        inventoryPage.addProductToCartByIndex(2);
+
+        CartPage cartPage = inventoryPage.goToCart();
+        Assert.assertEquals(cartPage.getCartItemsCount(), 3,
+                "Expected 3 items in the cart before removal.");
+
+        cartPage.removeAllItems();
+        Assert.assertEquals(cartPage.getCartItemsCount(), 0,
+                "The cart should be empty after removing all items.");
+    }
 }
